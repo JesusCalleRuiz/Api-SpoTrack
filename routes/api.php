70c4196cde4a1f59f3b08b3fb53062f0270f2b43
jesus.calle <jesus.calle@securitasdirect.es>
2024-11-18 14:01:48 +0100

    ruta info usuario autenticado

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+    return response()->json($request->user());
+});
