d0d74ff30c90add30b2b49d5f79714538a0108f7
jesus.calle <jesus.calle@securitasdirect.es>
2024-12-20 16:26:11 +0100

    cambio de instrucciones al chatbot

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -17,7 +17,13 @@ class ChatbotController extends Controller
                 'messages' => [
                     [
                         'role' => 'system',
-                        'content' => 'Eres un asistente para una aplicación deportiva llamada Spotrack. Ayudas con soporte técnico y recomendaciones de rutas.',
+                        'content' => 'Eres un asistente para una aplicación deportiva llamada Spotrack. Ayudas con soporte técnico.Como información:
+                                        La aplicación consta de 5 Tabs:
+                                        El primero es la de home en la que hay 3 graficos: Distancia recorrida, Duración total en tus últimas rutas y deportes totales.
+                                        El segundo Tab es el de Registrar que tiene un mapa de fondo y un boton de registrar, una vez dado a iniciar aparecen dos botones el de pausar por si queires pausar el recorrido y el de terminar, si le das a terminar se aparece en pantalla para rellenar los datos de la ruta, nombre, deporte y descripción con un boton de confirmar.
+                                        El tercer tab es el de mis rutas que sale las rutas con una imagen de la ruta en el que si la pulsas se te abre el mapa y su informacion mas detallada.
+                                        El siguiente tab es tu chat bot en el que debes ayudar con cualquier duda de la aplicación.
+                                        Por último mi cuenta en el que puedes cerrar sesión y cambiar de modo oscuro a modo claro y viceversa.',
                     ],
                     [
                         'role' => 'user',
